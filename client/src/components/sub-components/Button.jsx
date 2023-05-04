5ac074d42a5d1ca19780b5ccfad795ed4006a7e3
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-04 18:04:51 +0100

    Creating button component

---
+++ client/src/components/sub-components/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Button() {
+  return (
+    <div className="mb-3">
+      <input type="submit" className="btn btn-primary" value="Valider"></input>
+    </div>
+  );
+}
