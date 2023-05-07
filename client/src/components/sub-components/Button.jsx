556fd0bf58042ca317d66eb67ce1cf7a425ee21e
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-07 22:48:05 +0100

    Adding color prop

--- client/src/components/sub-components/Button.jsx
+++ client/src/components/sub-components/Button.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function Button() {
+export default function Button(props) {
   return (
     <div className="mb-3">
-      <input type="submit" className="btn btn-primary" value="Valider"></input>
+      <input
+        type="submit"
+        className={`btn btn-${props.color}`}
+        value="Valider"
+      ></input>
     </div>
   );
 }
