ce537585e0e834c55468e5012929871c2cd5e5b9
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-08 13:48:10 +0100

    Adding btn-lg and removing mb-3

--- client/src/components/sub-components/Button.jsx
+++ client/src/components/sub-components/Button.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 export default function Button(props) {
   return (
-    <div className="mb-3">
-      <input
-        type="submit"
-        className={`btn btn-${props.color}`}
-        value="Valider"
-      ></input>
-    </div>
+    <input
+      type="submit"
+      className={`btn btn-lg btn-${props.color}`}
+      value="Valider"
+    ></input>
   );
 }
