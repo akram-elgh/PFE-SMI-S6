b252997d1f7f3c62f97fa3ac9dd56f9542f688ba
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-08 22:51:21 +0100

    Adding updateStudent tab

--- client/src/components/main_components/Student.jsx
+++ client/src/components/main_components/Student.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import SpaceNavbar from "../sub-components/SpaceNavbar";
 import SearchStudent from "./student_components/SearchStudent";
+import UpdateStudent from "./student_components/UpdateStudent";
 
-export default function Student() {
+export default function Student(props) {
   const [activeTab, setActiveTab] = useState("search");
   const handleClick = (tabName) => setActiveTab(tabName);
   return (
     <div className="space">
       <SpaceNavbar onClick={handleClick} number="3"></SpaceNavbar>
       {activeTab === "search" && <SearchStudent></SearchStudent>}
-      {activeTab === "modify" && <div>Modify</div>}
+      {activeTab === "modify" && (
+        <UpdateStudent
+          showSuccessModal={props.showSuccessModal}
+          showFailModal={props.showFailModal}
+        ></UpdateStudent>
+      )}
       {activeTab === "delete" && <div>Delete</div>}
     </div>
   );
