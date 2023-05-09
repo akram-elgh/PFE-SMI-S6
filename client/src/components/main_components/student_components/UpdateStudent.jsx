2b73c7f25257977c27d98c42b5a92eda0e668b0c
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-09 19:01:02 +0100

    Improving UpdateStudent process

--- client/src/components/main_components/student_components/UpdateStudent.jsx
+++ client/src/components/main_components/student_components/UpdateStudent.jsx
@@ -9,18 +9,9 @@ export default function UpdateStudent(props) {
   const [classes, setClasses] = useState([]);
   const [students, setStudents] = useState([]);
   const [name, setName] = useState("");
-  const [student, setStudent] = useState({
-    id: 0,
-    fname: "",
-    lname: "",
-    bDate: "",
-    phoneNum: "",
-    parentNum: "",
-    level: 0,
-    class_id: 0,
-    new_class_id: 0,
-  });
+  const [student, setStudent] = useState({});
   const {
+    student_id,
     fname,
     lname,
     bDate,
@@ -60,12 +51,19 @@ export default function UpdateStudent(props) {
     if (event.target.checked) {
       const id = event.target.value;
       // const url = studentUrl + "?id=" + id;
-      axios.get("http://localhost:3001/student?id=" + id).then((response) =>
+      axios.get("http://localhost:3001/student?id=" + id).then((response) => {
         setStudent({
           ...response.data[0],
           new_class_id: response.data[0].class_id,
-        })
-      );
+        });
+        setStudents(response.data);
+      });
+    } else {
+      const url = studentUrl + "?name=" + name;
+      axios.get(url).then((response) => {
+        setStudents(response.data);
+        setStudent({});
+      });
     }
   }
 
@@ -78,17 +76,7 @@ export default function UpdateStudent(props) {
           props.showSuccessModal("L'etudiant ete modifier avec succes");
         else props.showFailModal("Erreur lors du modifcation de l'etudiant");
       });
-      setStudent({
-        id: 0,
-        fname: "",
-        lname: "",
-        bDate: "",
-        phoneNum: "",
-        parentNum: "",
-        level: 0,
-        class_id: 0,
-        new_class_id: 0,
-      });
+      setStudent({});
       setName("");
       setStudents([]);
     }
@@ -125,11 +113,12 @@ export default function UpdateStudent(props) {
                       <div className="form-check form-switch">
                         <input
                           type="checkbox"
-                          name="id"
+                          name="student_id"
                           className="form-check-input"
                           role="switch"
-                          value={student.id}
+                          value={student.student_id}
                           onChange={handleCheckChange}
+                          checked={student_id === student.student_id}
                         />
                       </div>
                     </td>
@@ -143,116 +132,120 @@ export default function UpdateStudent(props) {
           </table>
         </div>
       </div>
-      <div className="update-space-form">
-        <div className="space-form">
-          <div className="space-labels">
-            <ul>
-              <li className="space-lable-li">Nom:</li>
-              <li className="space-lable-li">Prenom:</li>
-              <li className="space-lable-li">Date de naissance:</li>
-              <li className="space-lable-li">Numero du telephone:</li>
-              <li className="space-lable-li">Numero du telephone du parent:</li>
-              <li className="space-lable-li">Niveau: </li>
-              <li className="space-lable-li">Classe: </li>
-            </ul>
-          </div>
-          <div className="space-inputs">
-            <form onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <input
-                  type="text"
-                  name="lname"
-                  placeholder="Taper ici"
-                  value={lname}
-                  className="form-control"
-                  // onChange={handleFormChange}
-                  disabled
-                ></input>
-              </div>
-              <div className="mb-4">
-                <input
-                  type="text"
-                  name="fname"
-                  placeholder="Taper ici"
-                  value={fname}
-                  className="form-control"
-                  // onChange={handleFormChange}
-                  disabled
-                ></input>
-              </div>
-              <div className="mb-4">
-                <input
-                  type="date"
-                  name="bDate"
-                  placeholder="Taper ici"
-                  value={bDate.slice(0, 10)}
-                  className="form-control"
-                  onChange={handleFormChange}
-                ></input>
-              </div>
-              <div className="mb-4">
-                <input
-                  type="text"
-                  name="phoneNum"
-                  placeholder="Taper ici"
-                  value={phoneNum}
-                  className="form-control"
-                  onChange={handleFormChange}
-                ></input>
-              </div>
-              <div className="mb-4">
-                <input
-                  type="text"
-                  name="parentNum"
-                  placeholder="Taper ici"
-                  value={parentNum}
-                  className="form-control"
-                  onChange={handleFormChange}
-                  required
-                ></input>
-              </div>
-              <div className="mb-4">
-                <select
-                  type="text"
-                  name="level"
-                  placeholder="Taper ici"
-                  value={level}
-                  className="form-select"
-                  onChange={handleFormChange}
-                >
-                  {getLevel().map((level, index) => {
-                    return (
-                      <option key={index} value={index}>
-                        {level}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div>
-              <div className="mb-4">
-                <select
-                  name="new_class_id"
-                  value={new_class_id}
-                  className="form-select"
-                  onChange={handleFormChange}
-                >
-                  <option value="0" key={0}>
-                    ---Selectioner une classe---
-                  </option>
-                  {classes.map((classe) => {
-                    return (
-                      <option key={classe.class_id} value={classe.class_id}>
-                        {classe.class_name}
-                      </option>
-                    );
-                  })}
-                </select>
-                <Button color="primary"></Button>
-              </div>
-            </form>
+      {student_id && (
+        <div className="update-space-form">
+          <div className="space-form">
+            <div className="space-labels">
+              <ul>
+                <li className="space-lable-li">Nom:</li>
+                <li className="space-lable-li">Prenom:</li>
+                <li className="space-lable-li">Date de naissance:</li>
+                <li className="space-lable-li">Numero du telephone:</li>
+                <li className="space-lable-li">
+                  Numero du telephone du parent:
+                </li>
+                <li className="space-lable-li">Niveau: </li>
+                <li className="space-lable-li">Classe: </li>
+              </ul>
+            </div>
+            <div className="space-inputs">
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="lname"
+                    placeholder="Taper ici"
+                    value={lname}
+                    className="form-control"
+                    // onChange={handleFormChange}
+                    disabled
+                  ></input>
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="fname"
+                    placeholder="Taper ici"
+                    value={fname}
+                    className="form-control"
+                    // onChange={handleFormChange}
+                    disabled
+                  ></input>
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="date"
+                    name="bDate"
+                    placeholder="Taper ici"
+                    value={bDate.slice(0, 10)}
+                    className="form-control"
+                    onChange={handleFormChange}
+                  ></input>
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="phoneNum"
+                    placeholder="Taper ici"
+                    value={phoneNum}
+                    className="form-control"
+                    onChange={handleFormChange}
+                  ></input>
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="parentNum"
+                    placeholder="Taper ici"
+                    value={parentNum}
+                    className="form-control"
+                    onChange={handleFormChange}
+                    required
+                  ></input>
+                </div>
+                <div className="mb-4">
+                  <select
+                    type="text"
+                    name="level"
+                    placeholder="Taper ici"
+                    value={level}
+                    className="form-select"
+                    onChange={handleFormChange}
+                  >
+                    {getLevel().map((level, index) => {
+                      return (
+                        <option key={index} value={index}>
+                          {level}
+                        </option>
+                      );
+                    })}
+                  </select>
+                </div>
+                <div className="mb-4">
+                  <select
+                    name="new_class_id"
+                    value={new_class_id}
+                    className="form-select"
+                    onChange={handleFormChange}
+                  >
+                    <option value="0" key={0}>
+                      ---Selectioner une classe---
+                    </option>
+                    {classes.map((classe) => {
+                      return (
+                        <option key={classe.class_id} value={classe.class_id}>
+                          {classe.class_name}
+                        </option>
+                      );
+                    })}
+                  </select>
+                  <Button color="primary"></Button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
