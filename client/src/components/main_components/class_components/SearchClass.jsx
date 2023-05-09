dbbfd276a0b5d8f0bb946099195005cee2cf9394
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-09 19:00:43 +0100

    Improving searchClass approach

--- client/src/components/main_components/class_components/SearchClass.jsx
+++ client/src/components/main_components/class_components/SearchClass.jsx
@@ -2,35 +2,14 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function SearchClass() {
-  const url = "http://localhost:3001/class?id=";
+  const url = "http://localhost:3001/class";
   const [classes, setClasses] = useState([]);
-  // eslint-disable-next-line no-unused-vars
-  const [classesTable, setClassesTable] = useState([]);
   useEffect(() => {
     axios.get(url).then((response) => setClasses(response.data));
   }, [url]);
 
-  function handleChange(event) {
-    const id = event.target.value;
-    axios.get(url + id).then((response) => setClassesTable(response.data));
-    setClassesTable([]);
-  }
   return (
     <div className="search-space">
-      <div className="search-space-input">
-        <select className="form-select" onChange={handleChange}>
-          <option value="0" key={0}>
-            ---Selectionez une classe---
-          </option>
-          {classes.map((classe) => {
-            return (
-              <option value={classe.class_id} key={classe.class_id}>
-                {classe.class_name}
-              </option>
-            );
-          })}
-        </select>
-      </div>
       <div className="search-space-table">
         <table className="table table-striped table-primary table-hover">
           <thead>
@@ -43,13 +22,14 @@ export default function SearchClass() {
             </tr>
           </thead>
           <tbody>
-            {classesTable.map((classe) => {
+            {classes.map((classe) => {
               return (
                 <tr key={classe.class_id}>
                   <td>{classe.class_name}</td>
                   <td>{classe.classroom}</td>
                   <td>{classe.duration === 90 ? "1h30" : "2h"}</td>
                   <td>{classe.price}DH</td>
+                  <td>{classe.fname}</td>
                 </tr>
               );
             })}
