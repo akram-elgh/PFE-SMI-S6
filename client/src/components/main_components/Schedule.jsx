80f8bdbb068910ed5738d08ed201d5f950da06ea
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-12 19:19:58 +0100

    Implementing the schedule space

--- client/src/components/main_components/Schedule.jsx
+++ client/src/components/main_components/Schedule.jsx
@@ -1,76 +1,95 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getDay, getHour } from "../functions/functions";
+import axios from "axios";
+import AddSchedule from "./schedule_components/AddSchedule";
 
-export default function schedule(props) {
+export default function Schedule(props) {
+  const days = getDay();
+  const hours = getHour();
+  const url = "http://localhost:3001/schedule?day=";
+  const postUrl = "http://localhost:3001/schedule?id=";
+  const [chosenDay, setChosenDay] = useState(1);
+  const [classes, setClasses] = useState([]);
+  const [chosenClass, setChosenClass] = useState(0);
+
+  useEffect(() => {
+    axios.get(url + chosenDay).then((response) => setClasses(response.data));
+  }, [url, chosenDay]);
+
+  function handleAddSubmit(id) {
+    axios.post(url + id).then((response) => {
+      if (response.status === 200) props.showSuccessModal("La classe ete ajouter dans l'emploi avec succes");
+      else props.showFailModal("Erreru lors du l'ajout du classe");
+    })
+  }
+  }
+
   return (
     <div className="space">
       <div className="shcedule search-space-table">
-        <table className="table table-hover table-light table-striped">
+        <table className="table schedule-table">
           <thead>
             <tr>
-              <td></td>
-              <td>Lundi</td>
-              <td>Mardi</td>
-              <td>Mercredi</td>
-              <td>Jeudi</td>
-              <td>Vendredi</td>
-              <td>Samedi</td>
+              <td key={0}></td>
+              {days.slice(1).map((day, index) => {
+                return (
+                  <td
+                    onClick={() => setChosenDay(index + 1)}
+                    className={`schedule-header ${
+                      chosenDay === index + 1 ? "active" : ""
+                    }`}
+                    style={{ padding: "0.5rem" }}
+                    key={index + 1}
+                  >
+                    {day}
+                  </td>
+                );
+              })}
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>10:30</td>
+              <td></td>
             </tr>
-            <tr>
-              <td>11:00</td>
-            </tr>
-            <tr>
-              <td>11:30</td>
-            </tr>
-            <tr>
-              <td>12:00</td>
-            </tr>
-            <tr>
-              <td>12:30</td>
-            </tr>
-            <tr>
-              <td>13:00</td>
-            </tr>
-            <tr>
-              <td>13:30</td>
-            </tr>
-            <tr>
-              <td>14:00</td>
-            </tr>
-            <tr>
-              <td>14:30</td>
-            </tr>
-            <tr>
-              <td>15:00</td>
-            </tr>
-            <tr>
-              <td>15:30</td>
-            </tr>
-            <tr>
-              <td>16:00</td>
-            </tr>
-            <tr>
-              <td>16:30</td>
-            </tr>
-            <tr>
-              <td>17:00</td>
-            </tr>
-            <tr>
-              <td>17:30</td>
-            </tr>
-            <tr>
-              <td>18:00</td>
-            </tr>
-            <tr>
-              <td>18:30</td>
-            </tr>
-            <tr>
-              <td>19:00</td>
-            </tr>
+            {hours.slice(1).map((hour, index) => {
+              return (
+                <>
+                  <tr
+                    key={index + 1}
+                    value={index + 1}
+                    className={index % 2 === 0 ? "odd" : ""}
+                  >
+                    <td>{hour}</td>
+                    {classes
+                      .filter((obj) => obj.hour === index + 1)
+                      .map((classe) => {
+                        return (
+                          <td
+                            key={classe.id}
+                            onClick={() => setChosenClass(classe.id)}
+                            className="schedule-td"
+                            value={classe.day}
+                          >
+                            <ul>
+                              <li>Classe: {classe.class_name}</li>
+                              <li>Salle: {classe.classroom}</li>
+                              <li>
+                                Termine le:{" "}
+                                {getHour(
+                                  (classe.duration === 120 ? 4 : 3) + index + 1
+                                )}
+                              </li>
+                            </ul>
+                          </td>
+                        );
+                      })}
+                    <td className="schedule-add-td">
+                      <AddSchedule onSubmit={handleAddSubmit}></AddSchedule>
+                    </td>
+                  </tr>
+                </>
+              );
+            })}
           </tbody>
         </table>
       </div>
