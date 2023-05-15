66899412c0d816dbb4727a96242ab8112752c724
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-15 17:23:08 +0100

    Adding finance login

---
+++ client/src/components/main_components/finance_components/FinanceLogin.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Button from "../../sub-components/Button";
+
+export default function FinanceLogin(props) {
+  const [password, setPassword] = useState("");
+  return (
+    <div className="finance-login">
+      {/* <div className="finance-login-title">
+        <h1>Entrer le mot du pass pour acceder a l'espace finance</h1>
+      </div> */}
+      <div className="finance-login-icon"></div>
+      <div className="finance-login-form">
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            props.onSubmit(password);
+            setPassword("");
+          }}
+        >
+          <input
+            type="password"
+            value={password}
+            placeholder="Entrer le mot du pass"
+            className="form-control mb-3"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <Button color="primary" size="md"></Button>
+        </form>
+      </div>
+      <div className="wave"></div>
+    </div>
+  );
+}
