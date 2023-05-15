a7496e2b475713b83bd6b5dbd6450bbe494226f8
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-15 23:44:09 +0100

    Adding the info and modify the finance body

--- client/src/components/main_components/finance_components/FinanceBody.jsx
+++ client/src/components/main_components/finance_components/FinanceBody.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 import FinanceNavBar from "./FinanceNavBar.jsx";
+import FinanceInfo from "./FinanceInfo.jsx";
+import FinanceModify from "./FinanceModify.jsx";
 
 export default function FinanceBody(props) {
   const [activeTab, setActiveTab] = useState("info");
   return (
     <div className="finance-body">
       <FinanceNavBar onClick={(tab) => setActiveTab(tab)}></FinanceNavBar>
-      {activeTab === "info" && <div>Info</div>}
-      {activeTab === "modify" && <div>modify</div>}
+      {activeTab === "info" && <FinanceInfo></FinanceInfo>}
+      {activeTab === "modify" && <FinanceModify></FinanceModify>}
     </div>
   );
 }
