9b03e63bb6dfc62a5a749d3a116f4d86f7088b42
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-18 18:05:52 +0100

    Adding HomeSchedule component and styling the home

---
+++ client/src/components/main_components/home_components/HomeSchedule.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { getDay, getHour } from "../../functions/functions";
+
+export default function HomeSchedule() {
+  const days = getDay();
+  const hours = getHour();
+  const url = "http://localhost:3001/schedule?day=";
+  const [chosenDay] = useState(new Date().getDay());
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    axios.get(url + chosenDay).then((response) => setClasses(response.data));
+  }, [url, chosenDay]);
+
+  return (
+    <>
+      <div className="home-space-schedule-table">
+        <table className="table table-light table-striped">
+          <thead>
+            <tr>
+              <td key={0}></td>
+              {days.slice(chosenDay, chosenDay + 1).map((day, index) => {
+                return (
+                  <td
+                    colSpan={10}
+                    onClick={() =>
+                      setScheduleInfo((prevValues) => {
+                        return { ...prevValues, chosenDay: index + 1 };
+                      })
+                    }
+                    style={{ padding: "0.5rem" }}
+                    key={index + 1}
+                  >
+                    {day}
+                  </td>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {hours.slice(1, 20).map((hour, index) => {
+              return (
+                <>
+                  <tr key={index + 1} value={index + 1}>
+                    <td>{hour}</td>
+                    {classes
+                      .filter((obj) => obj.hour === index + 1)
+                      .map((classe) => {
+                        return (
+                          <td key={classe.id} value={classe.day}>
+                            {classe.class_name}
+                          </td>
+                        );
+                      })}
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
