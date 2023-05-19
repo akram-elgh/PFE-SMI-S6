030de15c3ce753403e91e480a3d300e2fbaff323
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-19 12:16:27 +0100

    Storing the login in .env file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,10 +3,12 @@ import Login from "./login/Login";
 import Main from "./Main";
 
 export default function App() {
-  const [isSubmitted, setSubmit] = useState(true);
+  const [isSubmitted, setSubmit] = useState(false);
+  const username = import.meta.env.VITE_USERNAME;
+  const password = import.meta.env.VITE_PASSWORD;
 
   function handleSubmit(fullLogin) {
-    if (fullLogin.login === "admin" && fullLogin.password === "admin") {
+    if (fullLogin.login === username && fullLogin.password === password) {
       setSubmit(true);
     }
   }
