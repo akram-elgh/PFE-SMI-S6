497afa3f9a25c2850f9766d25bb3533e160d24b2
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-21 20:04:38 +0100

    Adding our mission section

---
+++ website/src/components/OurMission.jsx
@@ -0,0 +1,30 @@
+export default function OurMission() {
+  return (
+    <div className="our-mission row" id="our-mission">
+      <div className="our-mission-img col-6">
+        <img
+          src="/images/Teaching.svg"
+          alt="teacher image"
+          height="80%"
+          width="80%"
+        />
+      </div>
+      <div className="section-description col-6">
+        <div className="our-mission-title ">
+          <h1 className="row-title">Notre Mission</h1>
+        </div>
+        <div className="section-parag">
+          <p className="row-parag">
+            Notre mission depuis 16 ans est de fournir un enseignement de
+            qualité et un soutien académique exceptionnel. Chez Markaz Nour Al
+            Ilem, nous sommes dédiés à aider nos étudiants à développer leurs
+            compétences linguistiques et académiques. Avec notre équipe
+            expérimentée et notre engagement envers l&apos;excellence, nous
+            guidons nos étudiants vers la réussite et les préparons pour un
+            avenir prometteur.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
