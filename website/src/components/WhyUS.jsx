69f09f1c2e8d283d1dab4bcddd97d1270be0b9aa
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-21 20:04:54 +0100

    Adding why us section

---
+++ website/src/components/WhyUS.jsx
@@ -0,0 +1,28 @@
+export default function WhyUS() {
+  return (
+    <div className="why-us row" id="why-us">
+      <div className="col-6 section-description">
+        <div>
+          <h1 className="row-title">Pourquoi Markaz Nour Al Ilem</h1>
+        </div>
+        <div className="section-parag">
+          <p className="row-parag">
+            Chez Markaz Nour Al Ilem, nous avons les meilleurs enseignants de
+            Tetouan. Leur expertise dans toutes les matières est bien connue, ce
+            qui en fait le choix idéal pour votre apprentissage. Faites
+            confiance à nos enseignants expérimentés pour une expérience
+            d&apos;apprentissage exceptionnelle.
+          </p>
+        </div>
+      </div>
+      <div className="col-6 why-us-img">
+        <img
+          src="/images/Classroom.svg"
+          alt="classroom"
+          height="80%"
+          width="80%"
+        />
+      </div>
+    </div>
+  );
+}
