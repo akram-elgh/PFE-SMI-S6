5190d863b859f1c5f4464a69a647541177b0cfa5
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-05-22 14:38:31 +0100

    Adding our courses section

--- website/src/components/App.jsx
+++ website/src/components/App.jsx
@@ -2,6 +2,7 @@ import LandingPage from "./LandingPage.jsx";
 import OurMission from "./OurMission.jsx";
 import WhyUS from "./WhyUS.jsx";
 import Header from "./Header.jsx";
+import OurCourses from "./OurCourses.jsx";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <LandingPage></LandingPage>
       <OurMission></OurMission>
       <WhyUS></WhyUS>
+      <OurCourses></OurCourses>
     </div>
   );
 }
